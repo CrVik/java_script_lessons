9bc66e2212181f5d0af855133b20ac94307fc663
CrVik <crviker@gmail.com>
2019-02-17 00:40:50 +0600

    This is a firs commit

---
+++ main.js
@@ -0,0 +1,34 @@
+'use strict';
+
+// my first comment 
+
+/*
+    Many 
+    lines 
+    comment
+*/
+
+// alert('Hello world!');
+
+
+var leftBorderWight = 1;
+
+let second = 2;
+
+const pi = 3.14;
+
+var  obj = {
+    name: "John",
+    age: 22,
+    isMarried: false
+};
+
+console.log(obj["age"]);
+
+let arr = ['plum.png', '111.txt', 'apple.jpeg']
+
+console.log(arr[0]);
+
+let answer = confirm("Are you there?");
+
+console.log(answer);
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,21 @@
+
+// alert('Hello world!');
+
+
+var leftBorderWight = 1;
+
+let second = 2;
+
+const pi = 3.14;
+
+var  obj = {
+    name: "John",
+    age: 22,
+    isMarried: false
+};
+
+console.log(obj["age"]);
+
+let arr = ['plum.png', '111.txt', 'apple.jpeg']
+
+console.log(arr[0]);
